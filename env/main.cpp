cd99839f124eb56da42490b10bca4c84cfd0280b
romandion <romandion@163.com>
2018-06-15 13:50:03 +0800

    add env and math 's test

---
+++ env/main.cpp
@@ -0,0 +1,42 @@
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+
+int main(int argc , char * argv[])
+{
+    LPTSTR lpszVariable; 
+    LPTCH lpvEnv; 
+ 
+    // Get a pointer to the environment block. 
+ 
+    lpvEnv = GetEnvironmentStrings();
+
+    // If the returned pointer is NULL, exit.
+    if (lpvEnv == NULL)
+    {
+        printf("GetEnvironmentStrings failed (%d)\n", GetLastError()); 
+        return 0;
+    }
+ 
+    // Variable strings are separated by NULL byte, and the block is 
+    // terminated by a NULL byte. 
+
+    lpszVariable = (LPTSTR) lpvEnv;
+
+    while (*lpszVariable)
+    {
+        _tprintf(TEXT("%s\n"), lpszVariable);
+        lpszVariable += lstrlen(lpszVariable) + 1;
+    }
+    FreeEnvironmentStrings(lpvEnv);
+    return 1;
+}
+
---
+++ math/main.cpp
@@ -0,0 +1,19 @@
+
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+
+
+int main(int argc , char * argv[])
+{
+
+    BOOL v = FALSE;
+
+    ::printf("__LINE__ = %d \n" , __LINE__) ;
+
+    return 0 ;
+}
+
